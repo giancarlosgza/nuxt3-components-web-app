5495721605eed6256d99a768bdbee8391d476b45
Giancarlos Garza <giancarlosgza@gmail.com>
2022-11-24 15:41:25 -0600

    Folders setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+        head: {
+            title: 'Nuxt 3 Components',
+            titleTemplate: '%s | Nuxt 3 Components',
+            meta: [
+                {charset: 'utf-8'},
+                {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+                {hid: 'description', name: 'description', content: ''},
+            ],
+            link: [
+                {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
+                {
+                    rel: 'stylesheet',
+                    href: 'https://fonts.googleapis.com/css2?family=Material+Icons&family=Material+Icons+Outlined'
+                }
+            ]
+        },
 
+    }
 })
