716c15b1c90f212ad98367b1754b179f3c64689d
Giancarlos Garza <giancarlosgza@gmail.com>
2023-01-31 16:54:28 -0600

    Tables WIP

--- pages/components/tables.vue
+++ pages/components/tables.vue
@@ -27,29 +27,45 @@
                 <table class="table table-hover table-borderless">
                   <thead>
                   <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Handle</th>
+                    <th scope="col">UUID</th>
+                    <th scope="col">NOMBRE</th>
+                    <th scope="col">TIPO DE PUNTO</th>
+                    <th scope="col">FECHA DE CREACIÓN</th>
                   </tr>
                   </thead>
                   <tbody>
                   <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
+                    <td>bc43b7c2-30f1-430c-b009-e5d0fc3c36e1</td>
+                    <td>JULIO A. ROCA Y ROSETA</td>
+                    <td>
+                      <div class="badge badge-primary">
+                        <i class="material-icons-outlined me-1">&#xef56;</i>
+                        Punto de monitoreo
+                      </div>
+                    </td>
+                    <td>30/1/2023, 17:04:19</td>
                   </tr>
                   <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
+                    <td>618cebcf-e148-4c32-ac70-d446ed82fc5e</td>
+                    <td>PARQUE CHEPEVERA</td>
+                    <td>
+                      <div class="badge badge-success">
+                        <i class="material-icons-outlined me-1">&#xea63;</i>
+                        Parque
+                      </div>
+                    </td>
+                    <td>26/1/2023, 13:14:02</td>
                   </tr>
                   <tr>
-                    <th scope="row">3</th>
-                    <td colspan="2">Larry the Bird</td>
-                    <td>@twitter</td>
+                    <td>9ea061f4-bcaf-494d-a391-4e4d1ec0d3df</td>
+                    <td>Av. Gonzalitos y Pico de Orizaba</td>
+                    <td>
+                      <div class="badge badge-warning">
+                        <i class="material-icons-outlined me-1">&#xe565;</i>
+                        Puente
+                      </div>
+                    </td>
+                    <td>23/1/2023, 16:29:04</td>
                   </tr>
                   </tbody>
                 </table>
@@ -67,29 +83,45 @@
                 <table class="table table-hover table-striped">
                   <thead>
                   <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Handle</th>
+                    <th scope="col">UUID</th>
+                    <th scope="col">NOMBRE</th>
+                    <th scope="col">TIPO DE PUNTO</th>
+                    <th scope="col">FECHA DE CREACIÓN</th>
                   </tr>
                   </thead>
                   <tbody>
                   <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
+                    <td>bc43b7c2-30f1-430c-b009-e5d0fc3c36e1</td>
+                    <td>JULIO A. ROCA Y ROSETA</td>
+                    <td>
+                      <div class="badge badge-primary">
+                        <i class="material-icons-outlined me-1">&#xef56;</i>
+                        Punto de monitoreo
+                      </div>
+                    </td>
+                    <td>30/1/2023, 17:04:19</td>
                   </tr>
                   <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
+                    <td>618cebcf-e148-4c32-ac70-d446ed82fc5e</td>
+                    <td>PARQUE CHEPEVERA</td>
+                    <td>
+                      <div class="badge badge-success">
+                        <i class="material-icons-outlined me-1">&#xea63;</i>
+                        Parque
+                      </div>
+                    </td>
+                    <td>26/1/2023, 13:14:02</td>
                   </tr>
                   <tr>
-                    <th scope="row">3</th>
-                    <td colspan="2">Larry the Bird</td>
-                    <td>@twitter</td>
+                    <td>9ea061f4-bcaf-494d-a391-4e4d1ec0d3df</td>
+                    <td>Av. Gonzalitos y Pico de Orizaba</td>
+                    <td>
+                      <div class="badge badge-warning">
+                        <i class="material-icons-outlined me-1">&#xe565;</i>
+                        Puente
+                      </div>
+                    </td>
+                    <td>23/1/2023, 16:29:04</td>
                   </tr>
                   </tbody>
                 </table>
