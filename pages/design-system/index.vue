dcc888a0de460a8a517921c1f8197c8828db8dfc
Giancarlos Garza <giancarlosgza@gmail.com>
2023-04-13 17:58:10 -0600

    WIP

--- pages/design-system/index.vue
+++ pages/design-system/index.vue
@@ -1,94 +1,93 @@
 <template>
-  <div>
-    <!-- SECTION HEADER -->
-    <section class="section section-hero sc-gradient"></section>
+    <div>
+        <!-- SECTION HEADER -->
+        <section class="section section-hero sc-gradient"></section>
 
-    <!-- CARD HEADER TITLE -->
-    <div class="container container-overflow">
-      <div class="row">
-        <div class="col-md-12">
-          <div class="card">
-            <div class="card-body">
-              <p class="overline">C4 - MTY</p>
-              <h1>Design system</h1>
+        <!-- CARD HEADER TITLE -->
+        <div class="container container-overflow">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <p class="overline">C4 - MTY</p>
+                            <h1>Design system</h1>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
+
+        <!-- CONTENT -->
+        <div class="container mt-5">
+            <div class="row mb-3">
+                <div class="col-md-12">
+                    <h2 class="fw-bold">Theme surfaces</h2>
+                </div>
+            </div>
+            <div class="row mb-3">
+                <!-- SURFACES LIST -->
+                <div class="col-md-4 mb-3">
+                    <div class="card card-outline h-100" style="background: var(--theme-surface-100)">
+                        <div class="card-body bg-transparent">
+                            <p class="subtitle-1 fw-bold mb-3">--theme-surface-100</p>
+                            <p class="subtitle-1 mb-1">- Modal</p>
+                            <p class="subtitle-1 mb-1">- Sheets</p>
+                            <p class="subtitle-1 mb-1">- Sidebar</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-3">
+                    <div class="card card-outline h-100" style="background: var(--theme-surface-200)">
+                        <div class="card-body bg-transparent">
+                            <p class="subtitle-1 fw-bold mb-3">--theme-surface-200</p>
+                            <p class="subtitle-1 mb-1">- Datepicker</p>
+                            <p class="subtitle-1 mb-1">- Dropdown</p>
+                            <p class="subtitle-1 mb-1">- Tooltips</p>
+                            <p class="subtitle-1 mb-1">- Popups</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-3">
+                    <div class="card card-outline h-100" style="background: var(--theme-surface-300)">
+                        <div class="card-body bg-transparent">
+                            <p class="subtitle-1 fw-bold mb-3">--theme-surface-300</p>
+                            <p class="subtitle-1 mb-1">- Card</p>
+                            <p class="subtitle-1 mb-1">- Lists</p>
+                            <p class="subtitle-1 mb-1">- Pagination</p>
+                            <p class="subtitle-1 mb-1">- Placeholder</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-3">
+                    <div class="card card-outline h-100" style="background: var(--theme-surface-400)">
+                        <div class="card-body bg-transparent">
+                            <p class="subtitle-1 fw-bold mb-3">--theme-surface-400</p>
+                            <p class="subtitle-1 mb-1">- Badge</p>
+                            <p class="subtitle-1 mb-1">- Button</p>
+                            <p class="subtitle-1 mb-1">- Footer</p>
+                            <p class="subtitle-1 mb-1">- Pagination</p>
+                            <p class="subtitle-1 mb-1">- Tabs</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-3">
+                    <div class="card card-outline h-100" style="background: var(--theme-surface-500)">
+                        <div class="card-body bg-transparent">
+                            <p class="subtitle-1 fw-bold mb-3">--theme-surface-500</p>
+                            <p class="subtitle-1 mb-1">- Body</p>
+                            <p class="subtitle-1 mb-1">- Form control</p>
+                            <p class="subtitle-1 mb-1">- Navbar</p>
+                            <p class="subtitle-1 mb-1">- Maps control</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-
-    <!-- CONTENT -->
-    <div class="container mt-5">
-      <div class="row mb-3">
-        <div class="col-md-12">
-          <h2 class="fw-bold">Theme surfaces</h2>
-        </div>
-      </div>
-      <div class="row mb-3">
-        <!-- SURFACES LIST -->
-        <div class="col-md-4 mb-3">
-          <div class="card card-outline h-100" style="background: var(--theme-surface-100)">
-            <div class="card-body bg-transparent">
-              <p class="subtitle-1 fw-bold mb-3">--theme-surface-100</p>
-              <p class="subtitle-1 mb-1">- Modal</p>
-              <p class="subtitle-1 mb-1">- Sheets</p>
-              <p class="subtitle-1 mb-1">- Sidebar</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4 mb-3">
-          <div class="card card-outline h-100" style="background: var(--theme-surface-200)">
-            <div class="card-body bg-transparent">
-              <p class="subtitle-1 fw-bold mb-3">--theme-surface-200</p>
-              <p class="subtitle-1 mb-1">- Datepicker</p>
-              <p class="subtitle-1 mb-1">- Dropdown</p>
-              <p class="subtitle-1 mb-1">- Tooltips</p>
-              <p class="subtitle-1 mb-1">- Popups</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4 mb-3">
-          <div class="card card-outline h-100" style="background: var(--theme-surface-300)">
-            <div class="card-body bg-transparent">
-              <p class="subtitle-1 fw-bold mb-3">--theme-surface-300</p>
-              <p class="subtitle-1 mb-1">- Card</p>
-              <p class="subtitle-1 mb-1">- Lists</p>
-              <p class="subtitle-1 mb-1">- Pagination</p>
-              <p class="subtitle-1 mb-1">- Placeholder</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4 mb-3">
-          <div class="card card-outline h-100" style="background: var(--theme-surface-400)">
-            <div class="card-body bg-transparent">
-              <p class="subtitle-1 fw-bold mb-3">--theme-surface-400</p>
-              <p class="subtitle-1 mb-1">- Badge</p>
-              <p class="subtitle-1 mb-1">- Button</p>
-              <p class="subtitle-1 mb-1">- Footer</p>
-              <p class="subtitle-1 mb-1">- Pagination</p>
-              <p class="subtitle-1 mb-1">- Tabs</p>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4 mb-3">
-          <div class="card card-outline h-100" style="background: var(--theme-surface-500)">
-            <div class="card-body bg-transparent">
-              <p class="subtitle-1 fw-bold mb-3">--theme-surface-500</p>
-              <p class="subtitle-1 mb-1">- Body</p>
-              <p class="subtitle-1 mb-1">- Form control</p>
-              <p class="subtitle-1 mb-1">- Navbar</p>
-              <p class="subtitle-1 mb-1">- Maps control</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-  </div>
 </template>
 
 <script>
 export default {
-  name: "DesignSystemIndex"
+    name: "DesignSystemIndex"
 }
 </script>
--- pages/templates/index.vue
+++ pages/templates/index.vue
@@ -1,36 +1,36 @@
 <template>
-  <div>
-    <!-- SECTION HEADER -->
-    <section class="section section-hero sc-gradient"></section>
+    <div>
+        <!-- SECTION HEADER -->
+        <section class="section section-hero sc-gradient"></section>
 
-    <!-- CARD HEADER TITLE -->
-    <div class="container container-overflow">
-      <div class="row">
-        <div class="col-md-12">
-          <div class="card">
-            <div class="card-body">
-              <p class="overline">C4 - MTY</p>
-              <h1>Templates</h1>
+        <!-- CARD HEADER TITLE -->
+        <div class="container container-overflow">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <p class="overline">C4 - MTY</p>
+                            <h1>Templates</h1>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
-    </div>
 
-    <!-- CONTENT -->
-    <div class="container mt-5">
-      <div class="row mb-3">
-        <div class="col-md-12">
-          <h2 class="fw-bold">Examples</h2>
+        <!-- CONTENT -->
+        <div class="container mt-5">
+            <div class="row mb-3">
+                <div class="col-md-12">
+                    <h2 class="fw-bold">Examples</h2>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
 
-  </div>
+    </div>
 </template>
 
 <script>
 export default {
-  name: "TemplatesIndex"
+    name: "TemplatesIndex"
 }
 </script>
