b6a066c0d507e91735e0ee77136cfdb593727431
Giancarlos Garza <giancarlosgza@gmail.com>
2023-04-13 18:44:40 -0600

    Navigation menus methods

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -1,83 +1,109 @@
 <template>
-  <nav class="navbar navbar-expand-lg sticky-top">
-    <div class="container">
-      <NuxtLink to="/"
-                class="navbar-brand">
-        <span class="text-secondary">C4</span> - Nuxt 3
-      </NuxtLink>
-      <button class="navbar-toggler btn btn-link mx-0 "
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarNav"
-              aria-controls="navbarNav"
-              aria-expanded="false"
-              aria-label="Toggle navigation">
-        <i class="material-icons-outlined">&#xe5d2;</i>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav me-auto">
-          <li class="nav-item">
+    <nav class="navbar navbar-expand-lg sticky-top">
+        <div class="container">
             <NuxtLink to="/"
-                      class="nav-link" aria-current="page">
-              Dashboard
+                      class="navbar-brand">
+                <span class="text-secondary">C4</span> - Nuxt 3
             </NuxtLink>
-          </li>
-          <li class="nav-item">
-            <NuxtLink to="/components"
-                      class="nav-link">
-              Components
-            </NuxtLink>
-          </li>
-          <li class="nav-item">
-            <NuxtLink to="/templates"
-                      class="nav-link">
-              Templates
-            </NuxtLink>
-          </li>
-          <li class="nav-item">
-            <NuxtLink to="/design-system"
-                      class="nav-link">
-              Design System
-            </NuxtLink>
-          </li>
-        </ul>
-        <ul class="navbar-nav ms-auto">
-          <li class="nav-item">
-            <div class='dropdown'>
-              <button
-                  id='themeDropdown'
-                  class='nav-link btn btn-link ripple ripple-primary'
-                  type='button'
-                  data-bs-toggle='dropdown'
-                  aria-expanded='false'>
-                <i class='material-icons-outlined'
-                   data-bs-toggle='tooltip'
-                   data-bs-placement='bottom'
-                   title='Tema del sistema'>&#xe0f0;</i>
-              </button>
-              <IncludeColorModePicker/>
+            <button class="navbar-toggler btn btn-link mx-0 "
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation">
+                <i class="material-icons-outlined">&#xe5d2;</i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav me-auto">
+                    <li class="nav-item">
+                        <NuxtLink to="/"
+                                  class="nav-link" aria-current="page">
+                            Dashboard
+                        </NuxtLink>
+                    </li>
+                    <li class="nav-item">
+                        <NuxtLink to="/components"
+                                  class="nav-link">
+                            Components
+                        </NuxtLink>
+                    </li>
+                    <li class="nav-item">
+                        <NuxtLink to="/templates"
+                                  class="nav-link">
+                            Templates
+                        </NuxtLink>
+                    </li>
+                    <li class="nav-item">
+                        <NuxtLink to="/design-system"
+                                  class="nav-link">
+                            Design System
+                        </NuxtLink>
+                    </li>
+                </ul>
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <div class='dropdown'>
+                            <button
+                                    id='themeDropdown'
+                                    class='nav-link btn btn-link ripple ripple-primary'
+                                    type='button'
+                                    data-bs-toggle='dropdown'
+                                    aria-expanded='false'>
+                                <i class='material-icons-outlined'
+                                   data-bs-toggle='tooltip'
+                                   data-bs-placement='bottom'
+                                   title='Tema del sistema'>&#xe0f0;</i>
+                            </button>
+                            <IncludeColorModePicker/>
+                        </div>
+                    </li>
+                    <li class="nav-item navigation-menu"
+                        @click="toggleNavApps(); hideNavUser();">
+                        <button class="nav-link btn btn-link">
+                            <i class="material-icons-outlined">&#xe5c3;</i>
+                        </button>
+                        <NavigationApps :is-nav-apps-active="isNavAppsActive"/>
+                    </li>
+                    <li class="nav-item navigation-menu"
+                        @click="toggleNavUser(); hideNavApp();">
+                        <button class="nav-link btn btn-link">
+                            <i class="material-icons-outlined">&#xe853;</i>
+                        </button>
+                        <NavigationUser :is-nav-user-active="isNavUserActive"/>
+                    </li>
+                </ul>
             </div>
-          </li>
-          <li class="nav-item navigation-menu">
-            <button class="nav-link btn btn-link">
-              <i class="material-icons-outlined">&#xe5c3;</i>
-            </button>
-            <NavigationApps/>
-          </li>
-          <li class="nav-item navigation-menu">
-            <button class="nav-link btn btn-link">
-              <i class="material-icons-outlined">&#xe853;</i>
-            </button>
-            <NavigationUser/>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
+        </div>
+    </nav>
 </template>
 
 <script>
 export default {
-  name: "TheNavbar"
+    name: "TheNavbar",
+    data() {
+        return {
+            isNavAppsActive: false,
+            isNavUserActive: false,
+        }
+    },
+    methods: {
+        toggleNavApps() {
+            this.isNavAppsActive = !this.isNavAppsActive
+        },
+        toggleNavUser() {
+            this.isNavUserActive = !this.isNavUserActive
+        },
+        hideNavApp() {
+            this.isNavAppsActive = false
+        },
+        hideNavUser() {
+            this.isNavUserActive = false
+        },
+        hideNavs() {
+            this.hideNavApp()
+            this.hideNavUser()
+        }
+    }
 }
 </script>
--- components/navigation/User.vue
+++ components/navigation/User.vue
@@ -1,27 +1,33 @@
 <template>
-  <div>
-    <div class="nav-menu">
-      <div class="nav-menu-content">
-        <div class="nav-menu-header">
-          <p>Root Developer</p>
-          <p>root@c4mty.org</p>
+    <div>
+        <div :class="['nav-menu', isNavUserActive ? 'nav-active' : '']">
+            <div class="nav-menu-content">
+                <div class="nav-menu-header">
+                    <p>Root Developer</p>
+                    <p>root@c4mty.org</p>
+                </div>
+                <div class="nav-menu-body">
+                    <div class="nav-menu-item menu-account">
+                        <i class="material-icons-outlined">&#xf1b7;</i>
+                        <p title="Administrar mi cuenta">Administrar mi cuenta</p>
+                    </div>
+                </div>
+                <div class="nav-menu-footer">
+                    <button class="btn btn-default">Cerrar sesión</button>
+                </div>
+            </div>
         </div>
-        <div class="nav-menu-body">
-          <div class="nav-menu-item menu-account">
-            <i class="material-icons-outlined">&#xf1b7;</i>
-            <p title="Administrar mi cuenta">Administrar mi cuenta</p>
-          </div>
-        </div>
-        <div class="nav-menu-footer">
-          <button class="btn btn-default">Cerrar sesión</button>
-        </div>
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
 export default {
-  name: "NavigationUser"
+    name: "NavigationUser",
+    props: {
+        isNavUserActive: {
+            type: Boolean,
+            default: null
+        }
+    }
 }
 </script>
