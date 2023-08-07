10f79485851c8585fb4d5092fd5904e5131799a4
Giancarlos Garza <giancarlosgza@gmail.com>
2023-08-07 17:08:08 -0600

    Landing page template using scroll driven animations

--- pages/templates/index.vue
+++ pages/templates/index.vue
@@ -1,36 +1,59 @@
 <template>
-    <div>
-        <!-- SECTION HEADER -->
-        <section class="section section-hero sc-gradient"></section>
+  <div>
+    <!-- SECTION HEADER -->
+    <section class="section section-hero sc-gradient"></section>
 
-        <!-- CARD HEADER TITLE -->
-        <div class="container container-overflow">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="card">
-                        <div class="card-body">
-                            <p class="overline">C4 - MTY</p>
-                            <h1>Templates</h1>
-                        </div>
-                    </div>
-                </div>
+    <!-- CARD HEADER TITLE -->
+    <div class="container container-overflow">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="card">
+            <div class="card-body">
+              <p class="overline">C4 - MTY</p>
+              <h1>Templates</h1>
             </div>
+          </div>
         </div>
-
-        <!-- CONTENT -->
-        <div class="container mt-5">
-            <div class="row mb-3">
-                <div class="col-md-12">
-                    <h2 class="fw-bold">Examples</h2>
-                </div>
-            </div>
-        </div>
-
+      </div>
     </div>
+
+    <!-- TEMPLATES LIST -->
+    <div class="container mt-5">
+      <div class="row mb-3">
+        <div class="col-md-12">
+          <h2 class="fw-bold">Examples</h2>
+        </div>
+      </div>
+      <div class="row mb-3">
+        <!-- V FOR LIST -->
+        <div v-for="(element, index) in templateExamples"
+             :key="`element-${index}`"
+             class="col-6 col-md-3 mb-3">
+          <NuxtLink :to="`templates/${element.route}`"
+                    class="card card-no-border is-interactive">
+            <div class="card-body">
+              <p class="subtitle-1 mb-0">{{ element.name }}</p>
+            </div>
+          </NuxtLink>
+        </div>
+      </div>
+    </div>
+
+  </div>
 </template>
 
 <script>
 export default {
-    name: "TemplatesIndex"
+  name: "TemplatesIndex",
+  data() {
+    return {
+      templateExamples: [
+        {
+          name: 'Developer Tool Landing Page',
+          route: 'landing/developer'
+        }
+      ]
+    }
+  }
 }
 </script>
---
+++ pages/templates/landing/developer.vue
@@ -0,0 +1,150 @@
+<template>
+  <div>
+    <section class="section mt-5">
+      <div class="container">
+        <div class="row justify-content-center mb-5">
+          <div class="col-md-8 text-center">
+            <h1 class="mdc-78">BUILD. CODE. <span class="text-gradient orange">SHIP.</span></h1>
+            <p class="subtitle-1 text-muted lh-base my-3">
+              Streamline your documentation process with our cutting-edge developer tool designed to make writing and
+              managing documentation a breeze.
+            </p>
+
+            <button class="btn btn-primary py-3 px-4">Get started</button>
+            <button class="btn btn-default-outline py-3 px-4">Get a demo</button>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-12 text-center">
+            <img class="img-fluid scroll-driven animation-scale"
+                 src="https://images.pexels.com/photos/3861951/pexels-photo-3861951.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                 alt="Code image">
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <div class="container">
+      <div class="row mb-5">
+        <div class="col-md-6 align-self-center">
+          <i class="material-symbols-rounded material-symbols-rounded-bold icon-wrap icon-wrap-gradient mb-3">&#xe2cc;</i>
+          <h2 class="fw-bold">
+            <span class="text-gradient orange">Effortless</span> Documentation Creation
+          </h2>
+          <p class="subtitle-1 text-muted lh-base">
+            With DocWrite Pro, writing documentation becomes an intuitive and smooth
+            experience. Our user-friendly interface allows you to effortlessly draft, organize, and edit your content
+            with just a few clicks. Spend less time on formatting and more time on what matters – building exceptional
+            products.
+          </p>
+          <button class="btn btn-default-outline">
+            Learn more
+          </button>
+        </div>
+        <div class="col-md-6">
+          <img class="img-fluid scroll-driven animation-fade"
+               src="https://images.pexels.com/photos/3861963/pexels-photo-3861963.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+               alt="Code image">
+        </div>
+      </div>
+
+      <div class="row mb-5">
+        <div class="col-md-6">
+          <img class="img-fluid scroll-driven animation-reveal"
+               src="https://images.pexels.com/photos/3861958/pexels-photo-3861958.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+               alt="Code image">
+        </div>
+        <div class="col-md-6 align-self-center">
+          <i class="material-symbols-rounded material-symbols-rounded-bold icon-wrap icon-wrap-gradient mb-3">&#xef54;</i>
+          <h2 class="fw-bold">
+            Seamless
+            <span class="text-gradient orange">Integrations</span>
+          </h2>
+          <p class="subtitle-1 text-muted lh-base">
+            We understand that your work extends beyond our tool, and that's why DocWrite Pro seamlessly integrates with
+            a variety of popular apps and platforms. Whether you're using project management tools, version control
+            systems, or continuous integration services, you can easily connect DocWrite Pro and keep your documentation
+            in sync with your workflow.
+          </p>
+          <button class="btn btn-default-outline">
+            Learn more
+          </button>
+        </div>
+      </div>
+
+      <div class="row mb-5">
+        <div class="col-md-6">
+          <img class="img-fluid scroll-driven animation-reveal"
+               src="https://images.pexels.com/photos/3861961/pexels-photo-3861961.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+               alt="Code image">
+        </div>
+        <div class="col-md-6 align-self-center">
+          <i class="material-symbols-rounded material-symbols-rounded-bold icon-wrap icon-wrap-gradient mb-3">&#xe2c9;</i>
+          <h2 class="fw-bold">
+            Collaboration Made
+            <span class="text-gradient orange">EASY</span>
+          </h2>
+          <p class="subtitle-1 text-muted lh-base">
+            Collaboration is key in any development project, and DocWrite Pro facilitates smooth teamwork. Invite team
+            members to contribute, review, and comment on documentation in real-time. Embrace collective knowledge and
+            ensure your documentation stays up-to-date and comprehensive.
+          </p>
+          <button class="btn btn-default-outline">
+            Learn more
+          </button>
+        </div>
+      </div>
+
+      <div class="row mb-5">
+        <div class="col-md-6 align-self-center">
+          <i class="material-symbols-rounded material-symbols-rounded-bold icon-wrap icon-wrap-gradient mb-3">&#xf0c1;</i>
+          <h2 class="fw-bold">
+            Version
+            <span class="text-gradient orange">Control</span>
+          </h2>
+          <p class="subtitle-1 text-muted lh-base">
+            Our tool takes version control seriously. With DocWrite Pro, you can maintain a history of changes, revert
+            to previous versions, and track edits made by different contributors. Never lose track of your progress and
+            maintain a clear record of your documentation journey.
+          </p>
+          <button class="btn btn-default-outline">
+            Learn more
+          </button>
+        </div>
+        <div class="col-md-6">
+          <img class="img-fluid scroll-driven animation-fade"
+               src="https://images.pexels.com/photos/3861952/pexels-photo-3861952.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+               alt="Code image">
+        </div>
+      </div>
+    </div>
+
+    <section class="section bg-secondary">
+      <div class="container py-3">
+        <div class="row">
+          <div class="col-md-12">
+            <h2 class="fw-bold mb-3">Unlock your documents' full potential</h2>
+            <button class="btn btn-primary py-3 px-4">
+              Start your free trial
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<style scoped>
+.img-fluid {
+  border-radius: 25px;
+}
+</style>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "TemplatesLandingDeveloper"
+})
+</script>
